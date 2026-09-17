a79cb065724270c9762e8cd37b0deeda4d7e4ba7
.Norbi <75044724+Norbi1027@users.noreply.github.com>
2026-09-17 15:34:26 +0200

    Add files via upload

--- kepfeld/main.cpp
+++ kepfeld/main.cpp
@@ -16,44 +16,131 @@ void fakit2(const Mat img, Mat& dest) {
 	dest = img + 100;
 }
 
+void invertal(const Mat img, Mat& dest) {
+	if (img.channels() == 1) {
+		dest = 255 - img;
+	}
+	else {
+		dest = Scalar(255, 255, 255) - img;
+	}
+}
+
 int main() {
-	Mat img = imread("kep.jpg", IMREAD_UNCHANGED);
-
-	//Mat fekete = Mat::zeros(img.size(), img.type());
-	//Mat kep2(img.rows, img.cols, CV_8UC3); //pixelérték nem definiált
-	//kep2.setTo(Scalar(0, 255,0));
-
-	Mat img2;
-	fakit2(img,img2);
+	/*Mat fg = imread("kep.jpg", IMREAD_UNCHANGED);
+	Mat bg = imread("background.jpg", IMREAD_UNCHANGED);
 
 
-	/*if (img.empty()) {
+	if (fg.empty() || bg.empty()) {
 		cout << "nincs ott a kép" << endl;
 		exit(-1);
 	}
 
-	Mat img2;
-	//resize(img, img2, Size(200, 100));
-	resize(img, img2, Size(img.cols / 2, img.rows / 2));
+	resize(bg, bg, fg.size());
 
-	int t = img.type();
-	if (t == CV_8UC3) {
-		cout << "szines (24bit)" << endl;
-	}
-	else {
-		if (t == CV_8UC1) {
-			cout << "szürkeskála (8 bit)" << endl;
-		}
-		else {
-			cout << "egyeb" << endl;
-		}
+	int value = 50;
+	string winname = "eredmény";
+	imshow(winname, fg);
+	createTrackbar("alfa", winname, &value, 100);
+	
+	while (waitKey(1) != 'q') {
+		double a = value / 100.0;
+		Mat res = a * fg + (1 - a) * bg;
+		imshow("eredmény", res);
 	}
 	*/
-	imshow("kep1", img);
-	imshow("kep2", img2);
-	//imshow("kep2", fekete);
-	//imshow("kep3", kep2);
+
+
+
+	/*Mat fg = imread("kep.jpg", IMREAD_UNCHANGED);
+
+	if (fg.empty()) {
+		cout << "nincs ott a kép" << endl;
+		exit(-1);
+	}
+
+	int cut = 50;
+	Rect r(	cut, 
+			cut,
+			fg.cols-2*cut,
+			fg.rows-2*cut);
+	Mat small = fg(r);
+	imwrite("eredmeny.png", small);
+
+	
+	//waitKey();
+	*/
+
+	/*
+	Mat fg (400,600,CV_8UC3);
+	fg.setTo(Scalar(0, 0, 255));
+	Rect r(0, 0, fg.cols, fg.rows / 2);
+	fg(r).setTo(Scalar(0, 255, 255));
+
+	Mat logo = imread("griff_black.png", IMREAD_UNCHANGED);
+	resize(logo, logo, Size(300, 300));
+
+	Rect r2(fg.cols / 2 - logo.cols / 2,
+		fg.rows / 2 - logo.rows / 2,
+		logo.cols,
+		logo.rows);
+
+	Mat mid = fg(r2);
+	Mat mask = Scalar(255, 255, 255) - logo;
+
+
+	//honnan.copyTo(hova)
+	logo.copyTo(mid,mask);
+
+	imshow("inv", mask);
+	imshow("kozep", mid);
+	imshow("kep1", fg);*/
+
+	/*Mat fg = imread("background.jpg", IMREAD_COLOR);
+	Mat kep = imread("kep.jpg", IMREAD_COLOR);
+	resize(kep, kep, Size(200, 200));
+
+	Rect r2(fg.cols / 2 = kep.cols / 2,
+		fg.rows / 2 + 50,
+		kep.cols,
+		kep.rows);
+
+	Mat mid = fg(r2);
+	Mat mask = Scalar(255, 255, 255) - kep;
+
+	kep.copyTo(mid, mask);
+
+	imshow("inv", mask);
+	imshow("kozep", mid);
+	imshow("kep1", fg);
+
+	*/
+
+	Mat  kep = imread("kep.jpg", IMREAD_COLOR);
+	Mat bg = imread("background.jpg", IMREAD_COLOR);
+	resize(bg, bg, kep.size());
+
+	Vec3b w(255, 255, 255);
+
+	for (int i = 0; i < bg.rows; ++i) {
+		for (int j = 0; j < bg.cols;++j) {
+			if (kep.at<Vec3b>(i, j) != w) {
+				bg.at<Vec3b>(i, j) = kep.at<Vec3b>(i, j);
+			}
+		}
+	}
+
+	//Mat fg = imread("background.jpg", IMREAD_COLOR);
+	//cout << fg.at <Vec3b>(0, 0) << endl;
+	//cout << fg.at <Vec3b>(fg.rows - 1, fg.cols - 1) << endl;
+
+	//Mat fg2 = imread("background.jpg", IMREAD_COLOR);
+	//cout << fg2.at <Vec3b>(0, 0) << endl;
+	//cout << fg2.at <Vec3b>(fg2.rows - 1, fg2.cols - 1) << endl;
+
+	imshow("kep1",bg);
+
 	waitKey();
+
 
 	return 0;
 }
